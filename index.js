bd0906ff1c950839849197c7725002cbf4d8bb00
misraag <jaiofficial50@gmail.com>
2023-07-25 18:13:07 +0530

    Some modifications

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+
+let songs = [
+    {songName: "Song 1", filepath: 'songs/1.mp3', coverPath: 'covers/1.jpg'},
+    {songName: "Song 2", filepath: 'songs/2.mp3', coverPath: 'covers/2.jpg'},
+    {songName: "Song 3", filepath: 'songs/3.mp3', coverPath: 'covers/3.jpg'},
+    {songName: "Song 4", filepath: 'songs/4.mp3', coverPath: 'covers/4.jpg'},
+    {songName: "Song 5", filepath: 'songs/5.mp3', coverPath: 'covers/5.jpg'},
+    {songName: "Song 6", filepath: 'songs/6.mp3', coverPath: 'covers/6.jpg'},
+    {songName: "Song 7", filepath: 'songs/7.mp3', coverPath: 'covers/7.jpg'},
+    {songName: "Song 8", filepath: 'songs/8.mp3', coverPath: 'covers/8.jpg'},
+    {songName: "Song 9", filepath: 'songs/9.mp3', coverPath: 'covers/9.jpg'},
+    {songName: "Song 10", filepath: 'songs/10.mp3', coverPath: 'covers/10.jpg'}
+]
