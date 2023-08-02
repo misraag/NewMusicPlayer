f6fcc5598463d175988d86728e4a2d94787acadb
misraag <jaiofficial50@gmail.com>
2023-08-02 11:51:29 -0700

    Added js for timer and progress bar, some css changes in timer

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ let songIndex = 0;
 let audioElement = new Audio('songs/1.mp3');
 let songItem = Array.from(document.getElementsByClassName('cardcontent'));
 let masterPlay = document.getElementById('masterPlay');
+let masterPlayCover = Array.from(document.getElementsByClassName('masterPlayCover'));
 let masterSongName = document.getElementById('masterSongName');
+let timerStart = document.getElementById('timerstart');
+let timerEnd = document.getElementById('timerend');
+
 let songCard = Array.from(document.getElementsByClassName('cards'));
 let playIcon = Array.from(document.getElementsByClassName('play-icon'));
 
@@ -20,3 +24,66 @@ let songs = [
     {songName: "Song 10", filepath: 'songs/10.mp3', coverPath: 'covers/10.jpg'}
 ]
 
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime <=0){
+        audioElement.play();
+        masterSongName.innerText = songs[songIndex].songName;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        masterPlayCover[songIndex].classList.remove("fa-play-circle");
+        masterPlayCover[songIndex].classList.add("fa-pause-circle");
+
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+    }
+    
+    })
+    
+    function time(){
+        minsStart = parseInt((audioElement.currentTime)/60);
+        secondsStart = parseInt((audioElement.currentTime)-(60*minsStart));
+        minsEnd = parseInt((audioElement.duration - audioElement.currentTime)/60);
+        secondsEnd = parseInt((audioElement.duration - audioElement.currentTime) % 60);
+        if(audioElement.currentTime>59){
+            if (secondsStart<10) {
+                timerStart.innerText = `0${minsStart}:0${secondsStart}`;
+            } else {
+               timerStart.innerText = `0${minsStart}:${secondsStart}`; 
+            }
+
+            if (secondsEnd<10) {
+                timerEnd.innerText = `0${minsEnd}:0${secondsEnd}`;
+            } else {
+               timerEnd.innerText = `0${minsEnd}:${secondsEnd}`; 
+            }
+        }
+        else{
+            if (secondsStart<10) {
+                timerStart.innerText = `0${minsStart}:0${secondsStart}`;
+            } else {
+               timerStart.innerText = `0${minsStart}:${secondsStart}`; 
+            }
+
+            if (secondsEnd<10) {
+                timerEnd.innerText = `0${minsEnd}:0${secondsEnd}`;
+            } else {
+               timerEnd.innerText = `0${minsEnd}:${secondsEnd}`; 
+            }
+            
+        }
+        
+    }
+    
+    audioElement.addEventListener('timeupdate',()=>{
+        let progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+        myProgressBar.value = progress;
+        time();
+    })
+    
+    myProgressBar.addEventListener('change', ()=>{
+        audioElement.currentTime = ((myProgressBar.value)*(audioElement.duration))/100;
+        time();
+    })
