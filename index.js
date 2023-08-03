906386e6e9e367aa6b50b269bb8ef2a3ba26ff00
misraag <jaiofficial50@gmail.com>
2023-08-03 20:57:29 +0530

    Added js to display name and cover on load

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let songIndex = 0;
 let audioElement = new Audio('songs/1.mp3');
-let songItem = Array.from(document.getElementsByClassName('cardcontent'));
+let songItem = Array.from(document.getElementsByClassName('cards'));
 let masterPlay = document.getElementById('masterPlay');
 let masterPlayCover = Array.from(document.getElementsByClassName('masterPlayCover'));
 let masterSongName = document.getElementById('masterSongName');
@@ -24,8 +24,25 @@ let songs = [
     {songName: "Song 7", filepath: 'songs/7.mp3', coverPath: 'covers/7.jpg'},
     {songName: "Song 8", filepath: 'songs/8.mp3', coverPath: 'covers/8.jpg'},
     {songName: "Song 9", filepath: 'songs/9.mp3', coverPath: 'covers/9.jpg'},
-    {songName: "Song 10", filepath: 'songs/10.mp3', coverPath: 'covers/10.jpg'}
+    {songName: "Song 10", filepath: 'songs/10.mp3', coverPath: 'covers/10.jpg'},
+    {songName: "Song 11", filepath: 'songs/1.mp3', coverPath: 'covers/1.jpg'},
+    {songName: "Song 12", filepath: 'songs/2.mp3', coverPath: 'covers/2.jpg'},
+    {songName: "Song 13", filepath: 'songs/3.mp3', coverPath: 'covers/3.jpg'},
+    {songName: "Song 14", filepath: 'songs/4.mp3', coverPath: 'covers/4.jpg'},
+    {songName: "Song 15", filepath: 'songs/5.mp3', coverPath: 'covers/5.jpg'},
+    {songName: "Song 16", filepath: 'songs/6.mp3', coverPath: 'covers/6.jpg'},
+    {songName: "Song 17", filepath: 'songs/7.mp3', coverPath: 'covers/7.jpg'},
+    {songName: "Song 18", filepath: 'songs/8.mp3', coverPath: 'covers/8.jpg'},
+    {songName: "Song 19", filepath: 'songs/9.mp3', coverPath: 'covers/9.jpg'},
+    {songName: "Song 20", filepath: 'songs/10.mp3', coverPath: 'covers/10.jpg'},
+    {songName: "Song 21", filepath: 'songs/1.mp3', coverPath: 'covers/1.jpg'}
 ]
+
+songItem.forEach((element, i)=>{
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("music-name")[0].innerText = songs[i].songName;
+    let songElement = new Audio(`'${songs[i].filepath}'`);
+})
 
 masterPlay.addEventListener('click',()=>{
     if(audioElement.paused || audioElement.currentTime <=0){
@@ -99,6 +116,14 @@ masterPlay.addEventListener('click',()=>{
     // masterPlayCover.forEach((element)=> {
     //     element.addEventListener('click', (e)=>{
     //         songIndex = parseInt(e.target.id);
-    //         if(e.target.classList.contains)
-    //     })
+    //         if(e.target.classList.contains("fa-pause-circle")) {
+    //             audioElement.pause();
+    //             e.target.classList.remove("fa-pause-circle");
+    //             e.target.classList.add("fa-play-circle");
+    //             masterPlay.classList.remove("fa-pause-circle");
+    //             masterPlay.classList.add("fa-play-circle");
+    //         } else if () {
+
+    //         }
+    //      })
     // })
